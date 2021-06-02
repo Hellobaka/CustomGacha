ed78481bf60c0772ca5cab4a525ce984ee0accf2
Mr.喜 <50934714+Hellobaka@users.noreply.github.com>
2021-06-02 18:01:58 +0800

    UI - 托盘图标

--- me.cqp.luohuaming.CustomGacha.UI/Properties/Resources.Designer.cs
+++ me.cqp.luohuaming.CustomGacha.UI/Properties/Resources.Designer.cs
@@ -59,5 +59,15 @@ namespace me.cqp.luohuaming.CustomGacha.UI.Properties {
                 resourceCulture = value;
             }
         }
+        
+        /// <summary>
+        ///   查找类似于 (图标) 的 System.Drawing.Icon 类型的本地化资源。
+        /// </summary>
+        public static System.Drawing.Icon Icon {
+            get {
+                object obj = ResourceManager.GetObject("Icon", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
     }
 }
